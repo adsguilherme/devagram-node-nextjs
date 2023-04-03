82015a1dbcb950ec3fd8cdc6b8ef65b9cf3362db
gscode <adm.guilhermedesousa@gmail.com>
2023-04-03 11:21:25 -0300

    feature: resposta padrao na api e middleware

---
+++ middlewares/conectarMongoDB.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse, NextApiHandler } from 'next'
+import mongoose from 'mongoose'
+import type { respostaPadraoMsg } from '../types/respostaPadraoMsg'
+
+export const conectarMongoDB = (handler: NextApiHandler) =>
+  async (req: NextApiRequest, res: NextApiResponse<respostaPadraoMsg>) => {
+
+    // Verificar se o banco já está conectado, se estiver seguir para o endpoint ou próximo middleware.
+    if (mongoose.connections[0].readyState) {
+      return handler(req, res)
+    }
+
+    // Já que não está conectado vamos conectar. 
+    // Obter a variável de ambiente preenchida do .env
+    const { DB_CONEXAO_STRING } = process.env
+
+    // Se a env estiver vazia aborta o uso do sistema e aviso o programador.
+    if (!DB_CONEXAO_STRING) {
+      return res.status(500).json({ erro: 'Env de configuração do banco não informado.' })
+    }
+
+    mongoose.connection.on('connected', () => console.log('Banco de dados conectado.'))
+    mongoose.connection.on('error', error => console.log('Ocorreu um erro ao conectar no banco de dado.'))
+    await mongoose.connect(DB_CONEXAO_STRING)
+    // Agora posso segior para o endpoint, pois estou conectado no banco de dados.
+    return handler(req, res)
+}
